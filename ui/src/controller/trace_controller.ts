85afbaec79d13a7d3f728417b6be85ee1abf9055
Christian W. Damus <cdamus.ext@eclipsesource.com>
2023-10-16 18:46:52 -0400

    Highlight vsyncs in columns across all tracks (#52)
    For traces that have the VSYNC-app counter track in the SurfaceFlinger,
    support a 'v' key toggle to show the alignment of the vsyncs with other
    content in the tracks.
    
    Signed-off-by: Christian W. Damus <cdamus.ext@eclipsesource.com>

--- ui/src/controller/trace_controller.ts
+++ ui/src/controller/trace_controller.ts
@@ -120,6 +120,7 @@ import {
 import {TrackControllerArgs, trackControllerRegistry} from './track_controller';
 import {decideTracks} from './track_decider';
 import {VisualisedArgController} from './visualised_args_controller';
+import {VsyncController} from './vsync_controller';
 
 type States = 'init' | 'loading_trace' | 'ready';
 
@@ -328,6 +329,10 @@ export class TraceController extends Controller<States> {
         childControllers.push(
           Child('traceError', TraceErrorController, {engine}));
         childControllers.push(Child('metrics', MetricsController, {engine}));
+
+        // Vsync data from SurfaceFlinger's VSYNC-app counter, if available
+        childControllers.push(
+          Child('vsync', VsyncController, {engine, app: globals}));
 
         return childControllers;
 
